3894fb52c3b1142092ecc43bc4ccef658527d8b4
shev-81 <shev-81@mail.ru>
2022-03-30 13:21:10 +0300

    ДЗ-4

--- src/main/java/com/example/springapp/SpringAppApplication.java
+++ src/main/java/com/example/springapp/SpringAppApplication.java
@@ -3,6 +3,10 @@ package com.example.springapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *  Домашнее задание к 4 лекции  Шевеленко А.А.
+ */
+
 @SpringBootApplication
 public class SpringAppApplication {
 
