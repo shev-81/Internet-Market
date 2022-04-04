43d1ed6e41ac362c82b1c63cebfc8bf552a5f29e
shev-81 <shev-81@mail.ru>
2022-04-04 14:44:29 +0300

    Домашнее задание к 5 лекции

--- src/main/java/com/example/springapp/SpringAppApplication.java
+++ src/main/java/com/example/springapp/SpringAppApplication.java
@@ -1,5 +1,9 @@
 package com.example.springapp;
 
+/**
+ * Домашнее Зание к 5 лекции Основы веб-разработки на Spring Framework
+ */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +13,4 @@ public class SpringAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAppApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/springapp/controllers/MainController.java
+++ src/main/java/com/example/springapp/controllers/MainController.java
@@ -30,30 +30,6 @@ public class MainController {
     public void changePrice(@RequestParam Long productId, @RequestParam Integer delta){
         Product product = servicesProducts.getProductById(productId);
         product.changePrice(delta);
+        servicesProducts.save(product);
     }
-
-
-//    // localhost:8189/app/first
-//    @GetMapping("/first")
-//    @ResponseBody
-//    public String first(@RequestParam(required = false) int a, @RequestParam(name = "bb") int b){
-//        return  "сумма A + B = " + (a+b);
-//    }
-//
-//    // localhost:8189/app/product/{id}/info
-//    @GetMapping("/showProdId/{id}/info")
-//    @ResponseBody
-//    public String showProdId(@PathVariable Long id){
-//        return "Product" + id;
-//    }
-//
-//    @GetMapping("/product/{id}")
-//    public String showProd(Model model, @PathVariable Long id){
-//        Product prod = productRepository.findById(id);
-//        model.addAttribute("product", prod);
-//        return "product_page";
-//    }
-//
-
-
 }
